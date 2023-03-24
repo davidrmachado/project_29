5282d769bcd9ea15ac6da87018a36d94ba95aa2d
David-Neto <davidrmachado@gmail.com>
2023-03-24 02:20:08 -0300

    Entrega do requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui, por favor."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    logged_students = 0
+
+    for login, logout in permanence_period:
+        if type(login) != int or type(logout) != int:
+            return None
+        if login <= target_time <= logout:
+            logged_students += 1
+
+    return logged_students
