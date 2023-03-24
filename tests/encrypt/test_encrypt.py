d6ec0313c4f3eaddbf6c37b92b1294af056ecb89
David-Neto <davidrmachado@gmail.com>
2023-03-24 03:09:11 -0300

    Entrega do requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("bilbo", 3) == "lib_ob"
+    assert encrypt_message("bilbo", 9) == "oblib"
+    assert encrypt_message("bilbo", 1) == "b_obli"
+
+    with pytest.raises(TypeError):
+        encrypt_message("xablau", "0")
+
+    with pytest.raises(TypeError):
+        encrypt_message(7777, 1234)
